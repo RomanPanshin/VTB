581940930e275e6e904c6996826418b3a2dc23e3
Roman Panshin <roma.super@icloud.com>
2023-10-13 22:12:29 +0300

    JSON response

--- src/main/java/com/monkey/branchFinder/controller/PingController.java
+++ src/main/java/com/monkey/branchFinder/controller/PingController.java
@@ -3,11 +3,14 @@ package com.monkey.branchFinder.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.Map;
+
 @RestController
 public class PingController {
 
     @GetMapping("/ping")
-    public String ping() {
-        return "pong";
+    public Map<String, String> ping() {
+        return Collections.singletonMap("response", "pong");
     }
 }
