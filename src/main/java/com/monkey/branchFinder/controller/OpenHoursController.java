8c27f7eeb5ec205bdd891d3b4ee582c7a35fa3cb
Roman Panshin <roma.super@icloud.com>
2023-10-14 16:58:32 +0300

    added get cap by id branch

--- src/main/java/com/monkey/branchFinder/controller/OpenHoursController.java
+++ src/main/java/com/monkey/branchFinder/controller/OpenHoursController.java
@@ -5,15 +5,13 @@ import com.monkey.branchFinder.model.OpenHours;
 import com.monkey.branchFinder.service.OpenHoursService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.DayOfWeek;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/openhours/{branchId}/openhours")
+@RequestMapping("/api/openhours/{branchId}")
 public class OpenHoursController {
 
     private final OpenHoursService openHoursService;
@@ -23,10 +21,25 @@ public class OpenHoursController {
         this.openHoursService = openHoursService;
     }
 
-    @GetMapping
+    @GetMapping("/openhours")
     public ResponseEntity<List<OpenHours>> getOpenHoursByBranchId(@PathVariable Long branchId) {
 
         List<OpenHours> openHours = openHoursService.getOpenHoursByBranchId(branchId);
+
+        if (openHours.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(openHours);
+    }
+
+    @GetMapping("/byDayAndHour")
+    public ResponseEntity<List<OpenHours>> getOpenHoursByDayAndHour(
+            @PathVariable Long branchId,
+            @RequestParam DayOfWeek dayOfWeek,
+            @RequestParam Integer hour) {
+
+        List<OpenHours> openHours = openHoursService.getOpenHoursByDayAndHour(branchId, dayOfWeek, hour);
 
         if (openHours.isEmpty()) {
             return ResponseEntity.noContent().build();
